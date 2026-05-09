99ed30508fd0c6657f61bc78e5b7352f09a544e5
NanthuSSI <35912811+NanthuDev@users.noreply.github.com>
2026-05-09 20:17:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ import pickle
 
 with open('movie_data.pkl','rb') as file:
     movies,cosine_sim = pickle.load(file)
+
+
+    def get_recommendations(title, cosine_sim=cosine_sim):
+    idx = movies[movies['title'] == title].index[0]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+     
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:11] #to get 10 similar movies
+    movie_indices = [i[0] for i in sim_scores]
+    return  movies['title'].iloc[movie_indices]
